7e910b044ecda7f09036d03b427b3e0662b45c66
ICTER Guru <icterguru@gmail.com>
2018-11-07 23:49:56 -0500

    Calculator.java and CalculatorTest.java programs added

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package calculator;
+public class Calculator {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int minus(int a, int b) {
+		return a - b;  // ERROR!!!
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int modulo(int a, int b) {
+		return a % b;
+	}
+}
